cc1898005baa27cc5b4dc9c24b27bf8875a4d17e
PhillipS1024 <41407355+PhillipS1024@users.noreply.github.com>
2018-08-14 22:15:43 -0500

    Add files via upload

---
+++ GameRental/GameRental/Controllers/GameController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GameRental.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GameRental.Controllers
+{
+    public class GameController : Controller
+    {
+        IList<Game> List = new List<Game>() {
+                    new Game() { GameId = 1, GameName = "Destiny 2", Year = 2012, Price = 50 } ,
+               new Game() { GameId = 2,GameName = "God of War", Year = 2001,  Price = 20 } ,
+               new Game() { GameId = 3,GameName = "Grand Theft Auto", Year = 2014,  Price = 25 } ,
+               new Game() { GameId = 4,GameName = "Divison", Year = 2015 , Price = 25 } ,
+               new Game() { GameId = 5,GameName = "Road Blocks", Year = 2005 , Price = 5 } ,
+               new Game() { GameId = 4,GameName = "OverWatch", Year = 2013 , Price = 30 } ,
+               new Game() { GameId = 5,GameName = "NBA Live", Year = 2018 , Price = 50 }
+                };
+        
+        public ActionResult Index()
+        {
+            return View(List);
+        }
+
+        public ActionResult Edit(int Id)
+        {
+            
+            var std = List.Where(s => s.GameId == Id).FirstOrDefault();
+
+            return View(std);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Game std)
+        {
+             
+
+            return RedirectToAction("Index");
+        }
+
+    }
+
+
+
+
+
+
+
+
+}
---
+++ GameRental/GameRental/Models/Game.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GameRental.Models
+{
+    public class Game
+    {
+        public int GameId { get; set; }
+       
+        public string GameName{ get; set; }
+        public int Year { get; set; }
+        public int Price { get; set; }
+        public Standard standard { get; set; }
+    }
+}public class Standard
+{
+    public int StandardId { get; set; }
+    public string StandardName { get; set; }
+
+}
+
+
+
